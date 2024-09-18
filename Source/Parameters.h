f87cb7ccb5a1aaf7941db619085f26fe7b87a4b3
Andrea Casati <eisi@MacBookPro.station>
2024-09-18 20:39:44 +0200

    Master gain da -48 a +48 dB

--- Source/Parameters.h
+++ Source/Parameters.h
@@ -29,8 +29,8 @@
 // Plugin constants & ids
 
 #define GAIN_NAME                               "GAIN"
-#define GAIN_FLOOR                              -32.0f
-#define GAIN_CEILING                            32.0f
+#define GAIN_FLOOR                              -48.0f
+#define GAIN_CEILING                            48.0f
 #define GAIN_INTERVAL                           0.01f
 #define GAIN_DEFAULT                            0.0f
 #define SMOOTHING_TIME                          0.01f
