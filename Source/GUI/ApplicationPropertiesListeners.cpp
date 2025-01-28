59e9a46366184b821e6e9912302c674f6b690b56
Marco Tiraboschi <marcotiraboschi@hotmail.it>
2025-01-28 14:58:42 +0100

    :sparkles: Button listener for application properties

--- Source/GUI/ApplicationPropertiesListeners.cpp
+++ Source/GUI/ApplicationPropertiesListeners.cpp
@@ -1,12 +1,33 @@
 #include "ApplicationPropertiesListeners.h"
 
 // =============================================================================
+ApplicationPropertyButtonListener::ApplicationPropertyButtonListener(
+    const juce::String& pID, juce::ApplicationProperties& properties)
+    : propertyID(pID), applicationProperties(properties)
+{
+}
+
+// =============================================================================
+void ApplicationPropertyButtonListener::buttonClicked(juce::Button* b)
+{
+    if (juce::PropertiesFile* pf
+        = applicationProperties.getCommonSettings(true))
+        pf->setValue(propertyID, b->getToggleState());
+}
+void ApplicationPropertyButtonListener::buttonStateChanged(juce::Button* b) {}
+
+// =============================================================================
 ApplicationPropertiesButtonAttachment::ApplicationPropertiesButtonAttachment(
     juce::ApplicationProperties& properties, const juce::String& pID,
     std::shared_ptr<juce::Button> btn)
-    : applicationProperties(properties), propertyID(pID), button(btn)
+    : applicationProperties(properties)
+    , propertyID(pID)
+    , button(btn)
+    , buttonListener(pID, properties)
 {
+    button->addListener(&buttonListener);
 }
 ApplicationPropertiesButtonAttachment::~ApplicationPropertiesButtonAttachment()
 {
+    button->removeListener(&buttonListener);
 }
--- Source/GUI/ApplicationPropertiesListeners.h
+++ Source/GUI/ApplicationPropertiesListeners.h
@@ -2,6 +2,29 @@
 #include <JuceHeader.h>
 
 // =============================================================================
+/** Button listener for an application property */
+class ApplicationPropertyButtonListener : public juce::Button::Listener
+{
+public:
+    // =========================================================================
+    ApplicationPropertyButtonListener(const juce::String& propertyID,
+                                      juce::ApplicationProperties& properties);
+
+    //==========================================================================
+    virtual void buttonClicked(juce::Button*) override;
+    virtual void buttonStateChanged(juce::Button*) override;
+
+private:
+    // =========================================================================
+    juce::ApplicationProperties& applicationProperties;
+    juce::String propertyID;
+
+    // =========================================================================
+    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(
+        ApplicationPropertyButtonListener)
+};
+
+// =============================================================================
 /** Attachment for a button and an application property */
 class ApplicationPropertiesButtonAttachment
 {
@@ -17,6 +40,7 @@ private:
     juce::ApplicationProperties& applicationProperties;
     juce::String propertyID;
     std::shared_ptr<juce::Button> button;
+    ApplicationPropertyButtonListener buttonListener;
 
     // =========================================================================
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(
