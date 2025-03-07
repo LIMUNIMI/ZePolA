b819b600bd1b93cd42742027d5c232745778d8e0
Marco Tiraboschi <marcotiraboschi@hotmail.it>
2025-03-07 11:55:46 +0100

    :sparkles: Save/load presets

--- Source/GUI/TopMenuPanel.cpp
+++ Source/GUI/TopMenuPanel.cpp
@@ -3,8 +3,9 @@
 #include "ParameterPanel.h"
 
 // =============================================================================
-TopMenuPanel::TopMenuPanel(VTSAudioProcessor& processor)
-    : undoButton("UNDO",
+TopMenuPanel::TopMenuPanel(VTSAudioProcessor& p)
+    : processor(p)
+    , undoButton("UNDO",
                  juce::Drawable::createFromImageData(
                      BinaryData::anticlockwise_arrow_svg,
                      BinaryData::anticlockwise_arrow_svgSize)
@@ -29,6 +30,8 @@ TopMenuPanel::TopMenuPanel(VTSAudioProcessor& processor)
                      BinaryData::load_icon_svg, BinaryData::load_icon_svgSize)
                      .release(),
                  juce::Justification::centredLeft)
+    , presetLocation(juce::File::getSpecialLocation(
+          juce::File::SpecialLocationType::userDocumentsDirectory))
 {
     addAndMakeVisible(sep);
     addAndMakeVisible(undoButton);
@@ -49,7 +52,78 @@ TopMenuPanel::TopMenuPanel(VTSAudioProcessor& processor)
         = std::bind(&VTSAudioProcessor::undoManagerRedo, &processor);
     resetButton.onClick
         = std::bind(&VTSAudioProcessor::resetParameters, &processor);
+    exportButton.onClick = std::bind(&TopMenuPanel::exportParameters, this);
+    saveButton.onClick   = std::bind(&TopMenuPanel::saveParameters, this);
+    loadButton.onClick   = std::bind(&TopMenuPanel::loadParameters, this);
 }
+
+// =============================================================================
+void TopMenuPanel::updatePresetLocation(const juce::File& f)
+{
+    DBG("New preset location: '" << f.getFullPathName() << "'");
+    presetLocation = (f.existsAsFile()) ? f.getParentDirectory() : f;
+    DBG("  -> '" << presetLocation.getFullPathName() << "'");
+}
+void TopMenuPanel::saveParameters()
+{
+    juce::FileChooser chooser("Select the save location...", presetLocation,
+                              "*.xml");
+    if (chooser.browseForFileToSave(true))
+    {
+        auto file = chooser.getResult();
+        if (file.existsAsFile())
+            file.deleteFile();
+        else if (file.exists())
+        {
+            DBG("A directory was selected: '" << file.getFullPathName() << "'");
+            jassertfalse;
+            return;
+        }
+        juce::FileOutputStream outputStream(file);
+        if (outputStream.openedOk())
+        {
+            juce::MemoryBlock destData;
+            processor.getStateInformation(destData);
+            outputStream.write(destData.getData(), destData.getSize());
+            updatePresetLocation(file);
+        }
+        else
+        {
+            DBG("Something wrong happened while opening the file for writing: '"
+                << file.getFullPathName() << "'");
+            jassertfalse;
+        }
+    }
+}
+void TopMenuPanel::loadParameters()
+{
+    juce::FileChooser chooser("Select the preset to load...", presetLocation,
+                              "*.xml");
+    if (chooser.browseForFileToOpen())
+    {
+        auto file = chooser.getResult();
+        if (!file.existsAsFile())
+        {
+            DBG("File not found: '" << file.getFullPathName() << "'");
+            jassertfalse;
+            return;
+        }
+        juce::MemoryBlock srcData;
+        if (file.loadFileAsData(srcData))
+        {
+            processor.setStateInformation(srcData.getData(), srcData.getSize());
+        }
+        else
+        {
+            DBG("Something wrong happened while reading the file: '"
+                << file.getFullPathName() << "'");
+            jassertfalse;
+        }
+    }
+}
+void TopMenuPanel::exportParameters() { DBG("EXPORT PARAMETERS"); }
+
+// =============================================================================
 void TopMenuPanel::resized()
 {
     sep.setBounds(getLocalBounds());
--- Source/GUI/TopMenuPanel.h
+++ Source/GUI/TopMenuPanel.h
@@ -13,15 +13,28 @@ public:
     TopMenuPanel(VTSAudioProcessor&);
 
     // =========================================================================
+    /** Button callback for saving parameters */
+    void saveParameters();
+    /** Button callback for saving parameters */
+    void loadParameters();
+    /** Button callback for saving parameters */
+    void exportParameters();
+
+    // =========================================================================
     void resized() override;
 
 private:
     // =========================================================================
+    void updatePresetLocation(const juce::File&);
+
+    // =========================================================================
+    VTSAudioProcessor& processor;
     TextAndImageButton undoButton, redoButton, resetButton, exportButton,
         saveButton, loadButton;
     juce::Label autoGainLabel;
     juce::ToggleButton autoGainButton;
     SeparatorComponent sep;
+    juce::File presetLocation;
 
     // =========================================================================
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(TopMenuPanel)
