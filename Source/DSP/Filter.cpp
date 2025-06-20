26be132ab5124b2206bd5925c65d09bc18afe3e9
Marco Tiraboschi <marcotiraboschi@hotmail.it>
2025-06-20 15:37:04 +0200

    :fire: Remove old commented-out code for peak estimation

--- Source/DSP/Filter.cpp
+++ Source/DSP/Filter.cpp
@@ -239,29 +239,15 @@ std::complex<double> FilterElement::dtft(double omega) const
 {
     return _dtft_withGain(omega, gain);
 }
-// static double _wgl(double x)
-// {
-//     static const double c = -2.0 * exp(-0.5);
-//     return (x < 0.5) ? exp(-2.0 * x * x) : c * (x - 1.0);
-// }
 double FilterElement::rmsg() const
 {
     double g;
     if (type)
     {
-        // double r = getRealPart();
-        // double i = getImagPart();
         double a     = getAngle();
         auto one_mp2 = abs(1.0 - std::polar(coeffs[1], a + a));
-        // Approx v0
-        // |1 - |p|^2| * (Re^2{p} - 2|Re{z}| + Im^2{p} + 1)
-        // g = abs(1.0 - coeffs[1]) * (r * r + coeffs[0] + i * i + 1.0);
-        // Approx v1
         // |1 - |p|^2| * |1 - p^2|^2
         g = abs(1.0 - coeffs[1]) * one_mp2 * one_mp2;
-        // Approx v2
-        // W_GL(|p|) * |1 - p^2|^2
-        // g = _wgl(sqrt(coeffs[1])) * one_mp2 * one_mp2;
     }
     else
     {
